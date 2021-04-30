db6a17e8e31a72eda4e4d10d7dbc3d1516213b19
Tochukwu Ozurumba <ozurumbatochukwu@yahoo.com>
2021-04-30 16:14:29 +0100

    :star: initializing alphally project

---
+++ src/test/index.js
@@ -0,0 +1,9 @@
+import chai from 'chai';
+
+const { expect } = chai;
+
+describe('Initial test', () => {
+  it('should be equal to alphally-backend', () => {
+    expect('alphally-backend').to.equal('alphally-backend');
+  });
+});
